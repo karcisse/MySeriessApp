3d57c073eef64604839fdb0034a3d406e3643200
ASUSR75\Karol <cissewskikarol@gmail.com>
2017-02-20 00:33:15 +0100

    Initial commit. Need to do a lot of things

---
+++ app/src/main/java/com/karol/myseriesapp/SeriesListView.java
@@ -0,0 +1,23 @@
+package com.karol.myseriesapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by Karol on 19/02/2017.
+ */
+
+public class SeriesListView extends ListView {
+    public SeriesListView(Context context) {
+        super(context);
+    }
+
+    public SeriesListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SeriesListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
